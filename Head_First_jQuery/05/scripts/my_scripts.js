1b32d9e3cefd085a99a0c563d3dae8f8a8d5f1b2
lxvy <lxin0918@163.com>
2016-07-09 14:47:38 +0800

    20160709

--- Head_First_jQuery/05/scripts/my_scripts.js
+++ Head_First_jQuery/05/scripts/my_scripts.js
@@ -4,7 +4,7 @@ var headclix = 0, eyeclix = 0, noseclix = 0, mouthclix = 0;
 		lightning_one();
 		lightning_two();
 		lightning_three();
-	  $("#head").click(function(){
+$("#head").click(function(){
 		if (headclix < 9){
 	$("#head").animate({left:"-=367px"},500);
 		headclix+=1;
